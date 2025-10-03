2990eaac8514edd8beccc1024fcf3a54ee29d262
Christoph Lechleitner <christoph.lechleitner@iteg.at>
2025-10-03 14:51:37 +0200

    Compatibility with Django 4.x from Debian 13 Trixie hopefully without breaking Django 3.2 from Debian 12 Bookworm

--- wfs/urls.py
+++ wfs/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 from wfs.views import global_handler,related_handler
 
 # APP
 urlpatterns = [
-    url(r'^(?P<service_id>\d+)/$',global_handler, name='wfs'),
-    url(r'^(?P<service_id>\d+)/related/$',related_handler, name='wfs-related'),
+    re_path(r'^(?P<service_id>\d+)/$',global_handler, name='wfs'),
+    re_path(r'^(?P<service_id>\d+)/related/$',related_handler, name='wfs-related'),
 ]
